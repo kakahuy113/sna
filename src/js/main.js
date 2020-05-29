34bbee4d0f72d9d23a3ec2f8b52d783ec2163862
hoangviethung <hoangviet.hung.009@gmail.com>
2020-05-29 15:55:11 +0700

    update header

--- src/js/main.js
+++ src/js/main.js
@@ -4,14 +4,25 @@ import {
 } from './util/utilities';
 import Cookie from './lib/Cookie';
 
+const checkSubMenu = () => {
+	const items = document.querySelectorAll('.t_header--list-link>ul>li');
+	items.forEach((item) => {
+		const sub = item.querySelector('ul');
+		if (sub) {
+			item.classList.add('hasSub')
+		}
+	})
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 	Cookie();
 	getSVGs();
 	Loading();
+	// Check Sub Menu
+	checkSubMenu();
 });
 
 document.addEventListener('DOMContentLoaded', () => {});
-
 
 
 // CHECK FORM VALID
--- src/js/util/utilities.js
+++ src/js/util/utilities.js
@@ -1,7 +1,11 @@
 export const getSVGs = (selector) => {
-	const images =
-		Array.from(document.querySelectorAll(selector)) ||
-		Array.from(document.querySelectorAll('img.svg'));
+	let images;
+	if (selector) {
+		images = Array.from(document.querySelectorAll(selector));
+	} else {
+		images = Array.from(document.querySelectorAll('img.svg'));
+	}
+
 	for (let i = 0; i < images.length; i++) {
 		const url =
 			images[i].getAttribute('src') || images[i].getAttribute('data-src');
