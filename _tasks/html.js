b9f5f1e2331a350577c2a5d89b622cab31dde7f7
hoangviethung <hoangviet.hung.009@gmail.com>
2020-06-05 17:11:42 +0700

    upload 3 site

--- _tasks/html.js
+++ _tasks/html.js
@@ -1,11 +1,15 @@
-import { src, dest } from 'gulp'
+import {
+	src,
+	dest
+} from 'gulp'
 import pug from 'gulp-pug'
 import plumber from 'gulp-plumber'
 
 const htmlTask = () => {
 	return src('src/**.pug')
 		.pipe(
-			plumber(function(err) {
+			plumber(function (err) {
+				console.log(err);
 				this.emit('end')
 			})
 		)
